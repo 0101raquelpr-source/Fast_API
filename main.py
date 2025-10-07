9f2a681161f7aad6a045f740b9393571ffe85e69
pradaram <raquel.prada_ramos.ext@nokia.com>
2025-10-07 12:18:11 +0200

    Added new responses and  a model validator

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 
 from fastapi import FastAPI,Body,HTTPException, Path, Query
-from fastapi.responses import HTMLResponse
-from pydantic import BaseModel, Field
+from fastapi.responses import HTMLResponse,PlainTextResponse, RedirectResponse, FileResponse,JSONResponse
+from pydantic import BaseModel, Field, field_validator,model_validator
 from typing import Optional
 
 
@@ -17,8 +17,6 @@ class Movie(BaseModel):
     rating: float
     category: str
 
-
-
 class MovieCreate(BaseModel): 
     #Validations are created with Field(pydantic)
     id: int
@@ -33,6 +31,11 @@ class MovieCreate(BaseModel):
         "example": {'id':1,
                     'title':"Movie title", 'overview':"Movie overview",
                     'year':2023, 'rating':7.5, 'category':"Category"}}}'''
+    @model_validator(mode='after')
+    def title_must_be_different_from_overview(self) -> 'MovieCreate': 
+        if self.title == self.overview:
+            raise ValueError('The title cannot be identical to the synopsis (overview).')
+        return self
 
 class MovieUpdate(BaseModel):
     title: Optional[str] = None
@@ -46,12 +49,13 @@ movies:list[Movie] = []
 
 @app.get('/', tags=['Home'])
 def home():
-    return "HoMe"
+    return PlainTextResponse(content="Welcome to my app with FastAPI", status_code=200)
 
 
-@app.get('/movies', tags=['Movies'])
+@app.get('/movies', tags=['Movies'], response_description="List all movies")
 def get_all_movies() -> list[Movie]:
-    return [movie.model_dump() for movie in movies] 
+    content = [movie.model_dump() for movie in movies] 
+    return JSONResponse(content=content, status_code=200)
 
 
 @app.get('/movies/{id}', tags=['Movies'])
@@ -62,8 +66,8 @@ def get_movie(id:int = Path(gt=0)) -> Movie:
     raise HTTPException(status_code=404, detail="Movie not found")
 
 
-@app.get('/movies/', tags=['Movies'])
-def get_movie_by_category(category:str = Query(min_length=5,max_length=20)) -> list[dict]:
+@app.get('/movies/', tags=['Movies'], response_description="Movies filtered by category")
+def get_movie_by_category(category:str = Query(min_length=3,max_length=20)) -> list[dict]:
     search_term = category.lower()
     results = [
         m.model_dump() 
@@ -76,10 +80,11 @@ def get_movie_by_category(category:str = Query(min_length=5,max_length=20)) -> l
     
 
 
-@app.post('/movies', tags=['Movies'])
+@app.post('/movies', tags=['Movies'],response_description="Add a movie")
 def create_movie(movie: MovieCreate) -> list[Movie]:
     movies.append(movie)
-    return [movie.model_dump() for movie in movies] 
+    #return [movie.model_dump() for movie in movies] 
+    return RedirectResponse(url='/movies', status_code=303) #get all movies
 
 @app.put('/movies/{id}', tags=['Movies'])
 def update_movie(id:int, movie:MovieUpdate):
@@ -100,3 +105,7 @@ def delete_movie(id:int):
             movies.remove(m)
             return {'message': 'Movie deleted'}
     raise HTTPException(status_code=404, detail="Movie not found")
+
+@app.get('/get_file', tags=['Files'])
+def get_file():
+    return FileResponse('files/sample.pdf')
